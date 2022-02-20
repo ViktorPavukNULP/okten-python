83a14b2148f50cccc70be07761ffdd25b4263936
ViktorPavukNULP <72486611+ViktorPavukNULP@users.noreply.github.com>
2022-02-20 17:30:21 +0200

    homework2 fix

--- homework2/homework2.py
+++ homework2/homework2.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 from collections.abc import Callable
+
 # написать функцию на замыкания которая будет в себе хранить лист дел, вам нужно реализовать два метода
 # - первый записывает в эту переменную запись
 # - второй возвращает все записи
@@ -9,15 +12,13 @@ from collections.abc import Callable
 # 2) протипизировать первое задание
 
 def notebook() -> dict[str, Callable]:
-    todo_list = []
+    todo_list: list[str] = []
 
     def add_todo(todo: str) -> None:
         todo_list.append(todo)
 
-    def get_all() -> None:
-        print("Todo list:")
-        for row in todo_list:
-            print('-', row)
+    def get_all() -> list[str]:
+        return todo_list
 
     return {"add_todo": add_todo, "get_all": get_all}
 
@@ -28,26 +29,26 @@ notebook1["add_todo"]("Learn python")
 notebook1["add_todo"]("Learn javascript")
 notebook1["add_todo"]("Eat")
 notebook1["add_todo"]("Sleep")
-notebook1["get_all"]()
+print(notebook1["get_all"]())
 
 # 3) создать функцию которая будет возвращать сумму разрядов числа в виде строки (тоже с типизацией)
 
-def expanded_form(number: int) -> str:
-    number_string = str(number)
-    number_len = len(number_string)
-    result_string = ''
+def expanded_form(number: int | str) -> str:
+    number_string: str = str(number)
+    number_len: int = len(number_string)
+    result_string: str = ''
     for n, letter in enumerate(number_string):
         if letter == '0':
             continue
         result_string += letter+('0'*(number_len-n-1))
-        if n != number_len-1:
-            result_string += " + "
-    return result_string
+        result_string += " + "
+    return result_string[:-3]
 
 
 print(expanded_form(12))
 print(expanded_form(42))
 print(expanded_form(70304))
+print(expanded_form("10302910"))
 
 # Пример:
 #
