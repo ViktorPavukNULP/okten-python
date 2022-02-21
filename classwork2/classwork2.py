38c8c0161b4a406d289fc6cf150589c32f5d46a9
ViktorPavukNULP <72486611+ViktorPavukNULP@users.noreply.github.com>
2022-02-21 18:55:57 +0200

    classwork2 closure

--- classwork2/classwork2.py
+++ classwork2/classwork2.py
@@ -24,6 +24,43 @@ def func2():
     print('func2')
 
 
+# func1()
+# func2()
+# func1()
+# func2()
+
+# кому это очень легко то сделайте функцию на замыкания которая будет возвращать декоратор
+# который будет считать общее количество запущенных  функций декорированных этим декоратором
+
+def make_decorator():
+    counter = 0
+
+    def decorator(func):
+        def inner():
+            nonlocal counter
+            counter += 1
+            print('Counter:', counter)
+            func()
+            print('-' * 20)
+
+        return inner
+
+    return decorator
+
+
+decor = make_decorator()
+
+
+@decor
+def func1():
+    print('func1')
+
+
+@decor
+def func2():
+    print('func2')
+
+
 func1()
 func2()
 func1()
