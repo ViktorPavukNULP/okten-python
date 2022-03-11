4b64d76876a3f0f37a8c98348358e0cf06a46f9e
ViktorPavukNULP <72486611+ViktorPavukNULP@users.noreply.github.com>
2022-03-11 12:09:56 +0200

    homework 4 try except

--- homework4/homework4.py
+++ homework4/homework4.py
@@ -1,5 +1,10 @@
-with open('emails.txt', 'r') as rfile:
-    with open('gmails.txt', 'w') as wfile:
-        for line in rfile:
-            if line.split()[1].endswith('@gmail.com'):
-                wfile.write(line)
+try:
+    with open('emailss.txt', 'r') as rfile:
+        with open('gmails.txt', 'w') as wfile:
+            for line in rfile:
+                if line.split()[1].endswith('@gmail.com'):
+                    wfile.write(line)
+except FileNotFoundError:
+    print(FileNotFoundError)
+except Exception:
+    print(Exception)
